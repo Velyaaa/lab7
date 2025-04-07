beb46b6b6b9c37140570b3166ffc8ea4142bb0ba
Velyaaa <fdhgffhtd@gmail.com>
2025-04-07 22:23:02 +0300

    Add files via upload

---
+++ lab7/7lab/LinkedList.cs
@@ -0,0 +1,182 @@
+class LinkedList
+{
+    class Node
+    {
+        public double Value;
+        public Node? Next;
+
+        public Node(double value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+    private Node? head;
+    private int count = 0;
+
+    public LinkedList()
+    {
+        head = null;
+    }
+
+    public void PrintList()
+    {
+        Node? current = head;
+        while (current != null)
+        {
+            Console.Write(current.Value + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+
+    public void AddFirst(double value)
+    {
+        Node newNode = new Node(value);
+        newNode.Next = head;
+        head = newNode;
+        count++;
+    }
+
+    public void Clear()
+    {
+        head = null;
+        count = 0;
+    }
+
+
+    public void RemoveByIndex(int index)
+    {
+        if (index < 0 || index >= count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        if (index == 0)
+        {
+            head = head?.Next;
+        }
+        else
+        {
+            Node? current = head;
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current!.Next;
+            }
+
+            current!.Next = current.Next?.Next;
+        }
+        count--;
+    }
+
+    public double CalculateSum()
+    {
+        double sum = 0.0;
+        Node? current = head;
+        while (current != null)
+        {
+            sum += current.Value;
+            current = current.Next;
+        }
+        return sum;
+    }
+
+    public double CalculateAverage()
+    {
+        return CalculateSum() / Count;
+    }
+
+    public double? FindFirstGreaterElement()
+    {
+        double average = CalculateAverage();
+
+        Node? current = head;
+        while (current != null)
+        {
+            if (current.Value > average)
+            {
+                return current.Value;
+            }
+            current = current.Next;
+        }
+
+        return null;
+    }
+
+    public double FindSumOfGreaterElements(double key)
+    {
+        double sum = 0.0;
+        Node? current = head;
+        while (current != null)
+        {
+            if (current.Value > key)
+            {
+                sum += current.Value;
+            }
+            current = current.Next;
+        }
+
+        return sum;
+    }
+
+    public LinkedList FindElementsSmallerThanAverage()
+    {
+        LinkedList newList = new LinkedList();
+        double average = CalculateAverage();
+
+        Node? current = head;
+        while (current != null)
+        {
+            if (current.Value < average)
+            {
+                newList.AddFirst(current.Value);
+            }
+            current = current.Next;
+        }
+
+        return newList;
+    }
+
+    public void RemoveElementsAtEvenIndices()
+    {
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            if (i % 2 == 0)
+            {
+                RemoveByIndex(i);
+            }
+        }
+    }
+
+    public double this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            Node current = head!;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next!;
+            }
+
+            return current.Value;
+        }
+        set
+        {
+            if (index < 0 || index >= count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            Node current = head!;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next!;
+            }
+
+            current.Value = value;
+        }
+    }
+
+    public int Count => count;
+}
---
+++ lab7/7lab/Program.cs
@@ -0,0 +1,80 @@
+﻿class Program
+{
+    static void Main()
+    {
+        LinkedList list = new LinkedList();
+
+        list.AddFirst(1110.22);
+        list.AddFirst(3.57);
+        list.AddFirst(-964.25);
+        list.AddFirst(-73.23);
+        list.AddFirst(-2.82);
+        list.AddFirst(143.84);
+        list.AddFirst(4.96);
+        list.AddFirst(-77.96);
+
+
+
+
+        /***************************************************************************************************
+        /* Shift + Alt + A  -  бінд на коментування */
+       
+
+        /* list.AddFirst(1110.22);
+        list.AddFirst(465.25);
+        list.AddFirst(3.57);
+        list.AddFirst(73.23);
+        list.AddFirst(2.82);
+        list.AddFirst(143.84);
+        list.AddFirst(4.96);
+        list.AddFirst(77.96); */
+
+        Console.WriteLine("\n<------------------------------------------------------>");
+        Console.WriteLine("Створений список:");
+        list.PrintList();
+
+        Console.WriteLine($"\n1. Перший елемент більший за середнє значення ({list.CalculateAverage():F2}): {list.FindFirstGreaterElement()}");
+
+        Console.WriteLine($"\nСума всіх елементів: {list.CalculateSum():F2}");
+
+        Console.WriteLine($"\n2. Сума елементів, значення яких більше за задане значення ({59}): {list.FindSumOfGreaterElements(59)}:F2");
+
+        LinkedList newList = list.FindElementsSmallerThanAverage();
+        Console.WriteLine($"\n3. Новий список з елементів, що менші за середнє ({list.CalculateAverage():F2}):");
+        newList.PrintList();
+
+
+        Console.WriteLine($"\n4. Список після видалення елементів з парними індексами:");
+        list.RemoveElementsAtEvenIndices();
+        list.PrintList();
+
+        Console.WriteLine();
+        list.PrintList();
+        
+        try
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"Елемент з індексом {i}: {list[i]}");
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Помилка: індекс поза межами списку!");
+        }
+
+        int index = 2;
+
+        try
+        {
+            list.RemoveByIndex(index);
+            Console.WriteLine($"\n\nСписок після видалення елемента з індексом: {index}:");
+            list.PrintList();
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Помилка: індекс поза межами списку!");
+        }
+    }
+}
+
